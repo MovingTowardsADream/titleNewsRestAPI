45f608f14b1b880468317984e3de94dddf406f30
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-03-29 23:28:37 +0300

    configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
 	news "titleNewsRestApi"
 	"titleNewsRestApi/pkg/handler"
 	"titleNewsRestApi/pkg/repository"
@@ -12,13 +15,21 @@ import (
 func main() {
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("Failed reading config")
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("Failed reading db password")
+	}
+
 	db, err := repository.NewPostgresDB(repository.ConfigDB{
-		Host:     "localhost",
-		Port:     "5432",
-		Username: "postgres",
-		Password: "qwerty",
-		DBName:   "postgres",
-		SSLMode:  "disable",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.name"),
+		SSLMode:  viper.GetString("db.ssl_mode"),
 	})
 	if err != nil {
 		logrus.Fatalf("Failed initialization database")
@@ -29,7 +40,13 @@ func main() {
 	handlers := handler.NewHandler(serv)
 
 	server := new(news.Server)
-	if err := server.Run("8000", handlers.InitHandler()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitHandler()); err != nil {
 		logrus.Fatal("Error server")
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
