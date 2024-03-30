33746b9eaaf06513c0cb5a61ab6f683241415111
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-03-30 19:18:41 +0300

    tests

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -5,6 +5,8 @@ import (
 	"titleNewsRestApi/pkg/repository"
 )
 
+//go:generate mockgen -source=service.go -destination=mocks/mock.go
+
 type Authorization interface {
 	CreateUser(user news.User) (int, error)
 	GenerateToken(username, password string) (string, error)
